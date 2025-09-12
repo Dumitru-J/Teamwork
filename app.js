1b272fcc60ca687d21d3338c837c3ef0790b9116
Haupt Alexander <alexander.haupt@htlpinkafeld.at>
2025-09-12 19:11:23 +0200

    feat: Add section B content to main

---
+++ app.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const users = [];
+
+app.post('/users', (req, res) => {
+    const user = req.body;
+    users.push(user);
+    res.status(201).send(user);
+});
+
+app.post('/api/user', (req, res) => {
+  // Query-Parameter aus der URL
+  const role = req.query.role;
+  console.log(`Empfangene Rolle: ${role}`);
+  // Daten aus dem Request-Body
+  const { name, age } = req.body;
+  res.json({
+    message: `Benutzer ${name} (${age}) mit Rolle ${role} wurde empfangen.`,
+    data: { name, age, role }
+  });
+});
+
+
+app.get('/users', (req, res) => {
+    res.send(users);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+ 
